aa623557836e25e4e43e26264c328d0233814a41
nathansivalingam <nathansivalingampersonal@gmail.com>
2024-11-02 17:15:37 +1100

    safety commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { BrowserRouter, Navigate, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import Register from './page/register';
 import Login from './page/login';
+import Logout from './component/logout';
 
 const Homepage = <>a</>;
 const Dashboard = <>dashboard</>;
@@ -12,27 +12,12 @@ const Dashboard = <>dashboard</>;
 function App() {
 
   const [token, setToken] = useState(null);
-  // const navigate = useNavigate();
 
   React.useEffect(() => {
     if (localStorage.getItem('token') != null) {
       setToken(localStorage.getItem('token'));
     }
   }, []);
-
-  const logout = () => {
-    axios.post('http://localhost:5005/admin/auth/logout', {}, {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-    .then( (response) => {
-      localStorage.removeItem('token');
-      setToken(null);
-      navigate('/login');
-    })
-    .catch( (error) => {
-      console.log(error.response.data.error);
-    });
-  }
 
   return (
     <>
@@ -41,7 +26,7 @@ function App() {
           {token ? (
             <>
               <Link to='/dashboard'>Dashboard</Link>
-              <button onClick={logout}>Logout</button>
+              <Logout token={token} setToken={setToken} />
             </>
           ) : (
             <>
