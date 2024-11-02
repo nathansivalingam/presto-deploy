eff4fd10b355126e47c19029d6122f71201e8ee8
Ben <benmcmillen12@gmail.com>
2024-11-02 19:40:05 +1100

    Dealing with passwords not matching on register

--- frontend/src/page/register.jsx
+++ frontend/src/page/register.jsx
@@ -6,11 +6,16 @@ import { useNavigate } from 'react-router-dom';
 function Register({ token, handleSuccess }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confPassword, setConfPassword] = useState('');
   const [name, setName] = useState('');
   const [error, setError] = useState(null);
 
   const register = () => {
-    console.log(email, password, name);
+    console.log(email, password, confPassword, name);
+    if (confPassword !== password){
+      setError("Passwords do NOT match");
+      return;
+    }
     axios.post('http://localhost:5005/admin/auth/register', {
       email: email,
       password: password,
@@ -31,6 +36,7 @@ function Register({ token, handleSuccess }) {
       <h2>Register</h2>
       Email: <input type="text" value={email} onChange={e => setEmail(e.target.value)} /><br />
       Password: <input type="text" value={password} onChange={e => setPassword(e.target.value)} /><br />
+      Confirm Password: <input type="text" value={confPassword} onChange={e => setConfPassword(e.target.value)} /><br />
       Name: <input type="text" value={name} onChange={e => setName(e.target.value)} /><br />
       {error && <div style={{color: 'red'}}>Error: {error} </div>}
       <button onClick={register}>Register</button>
