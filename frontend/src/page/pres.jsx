7797690475de4570655a834c897632a7accbdd37
Ben <benmcmillen12@gmail.com>
2024-11-07 00:27:50 +1100

    Functioning out pres code

--- frontend/src/page/pres.jsx
+++ frontend/src/page/pres.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { CurSlide, PresPage } from '../styles/styledComponents';
+import React, { useState } from 'react';
 
 const Pres = function ({ token, curStore, setStoreFn }) {
-
+    const [curSlideNum, setCurSlideNum] = useState(0);
     const params = useParams();
     const newStore = {...curStore};
     const firstSlide = newStore?.allPres?.[params.presid]?.slides?.[0];
 
+
+    const displayCurSlide = () => {
+        return <CurSlide>
+                This is the firsts slide = {curSlideNum}
+            </CurSlide>
+    }
+
+
     return <>
         <PresPage>
-            <CurSlide>
-                This is the first slide = {firstSlide}
-            </CurSlide>
+            {displayCurSlide()}
         </PresPage>
+
+        <div>
+            dwadad
+        </div>
     </>;
 
 
