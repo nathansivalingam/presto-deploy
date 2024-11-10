c59d93b96d883e15e89dac650457bda1276e2e0d
Nathan Sivalingam <z5359644@ad.unsw.edu.au>
2024-11-10 12:04:36 +1100

    tried out eslint and modified 'logout' based on input - safety commit in case I make too many changes

--- frontend/src/component/logout.jsx
+++ frontend/src/component/logout.jsx
@@ -2,23 +2,22 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const Logout = ({token, setToken}) => {
-    const navigate = useNavigate();
-    
-    const logout = () => {
-        axios.post('http://localhost:5005/admin/auth/logout', {}, {
-            headers: { Authorization: `Bearer ${token}` }
-        })
-        .then( (response) => {
-            localStorage.removeItem('token');
-            setToken(null);
-            navigate('/');
-        })
-        .catch( (error) => {
-            console.log(error.response.data.error);
-        });
-    }
+  const navigate = useNavigate();
+  const logout = () => {
+    axios.post('http://localhost:5005/admin/auth/logout', {}, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    .then( (response) => {
+      localStorage.removeItem('token');
+      setToken(null);
+      navigate('/');
+    })
+    .catch( (error) => {
+      console.log(error.response.data.error);
+    });
+  }
 
-    return <button onClick={logout}>Logout</button>
+  return <button onClick={logout}>Logout</button>
 }
 
 export default Logout;
